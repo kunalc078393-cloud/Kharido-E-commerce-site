1eb78b3fb54a10e4ff5889c2d86c9ba9f85dde3d
Rajshekhar <rajashekharhugar1718@gmail.com>
2026-07-04 11:45:14 +0530

    ADD script js.j

---
+++ script.js
@@ -0,0 +1,23 @@
+let siteName = "Kharido";
+console.log(siteName);
+
+let userName = "raj";
+console.log(userName);
+
+let userAge = 18;
+console.log(userAge);
+
+let userEmail = "you@example.com";
+console.log(userEmail);
+
+let password = "password123";
+console.log(password);
+alert("dt77d7");
+let isLoggedIn = true;
+console.log(isLoggedIn);
+
+let productPrice = "$100";
+console.log(productPrice);
+
+let productQuantity = 5;
+console.log(productQuantity);
